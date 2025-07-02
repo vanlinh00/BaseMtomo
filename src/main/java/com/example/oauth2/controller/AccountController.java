89f88d1610f1e901d622b294cd40917943b9b187
OMINEXT\linhnv03 <linhnv03@ominext.com>
2025-07-02 17:52:48 +0700

    Initial commit

---
+++ src/main/java/com/example/oauth2/controller/AccountController.java
@@ -0,0 +1,27 @@
+package com.example.oauth2.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AccountController {
+
+    //    @PreAuthorize("@roleChecker.hasValidRole(#principal)")
+    //@PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/me",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> user(Principal principal) {
+        System.out.println(principal);
+
+//        JsonObject jsonObject = new JsonObject();
+//        jsonObject.addProperty("messages",
+//                String.format("Welcome again `%s`. And Happy nice day!", principal.getName()));
+        return new ResponseEntity<>("hihi"/*jsonObject.toString()*/, HttpStatus.FOUND);
+    }
+}
